32366bafbe8f8159c434200564db0fb698a908e2
laitsky <vincent.diamond15@gmail.com>
2019-10-20 00:03:33 +0700

    UPDATE: pti-week8 (DONE), sbd-week8 (DONE).

--- Pengantar Teknologi Internet (IF300 - BL)/27047_VincentDiamond_Week08/script.js
+++ Pengantar Teknologi Internet (IF300 - BL)/27047_VincentDiamond_Week08/script.js
@@ -1,9 +1,12 @@
-$(document).ready(documentReady());
+$(document).ready(documentReady);
 
 function documentReady() {
     $("#accordion").accordion({
         collapsible: true
     });
+
+    $('#price').prop('disabled', true);
+    let roomName;
 
     let dialog = $("#dialog-form").dialog({
         autoOpen: false,
@@ -13,15 +16,41 @@ function documentReady() {
         buttons: {
             "Add new order": function () {
                 //implement here
+                $('#tableOrder').append(
+                    '<tr><td>' + $('#name').val() + '</td>' +
+                    '<td>' + roomName + '</td>' +
+                    '<td>$' + $('#deposit').val() + '</td></tr>'
+                );
             },
             Cancel: function () {
                 dialog.dialog("close");
             }
+        },
+        close: function () {
+            $('form')[0].reset();
         }
     });
 
     $(".btnAddOrder").on("click", function (btn) {
         //and here
+        let roomTypeID = $(this).attr('room-type');
+
+        if (roomTypeID == "single-room") {
+            $('#price').val(200);
+            roomName = "Griffin";
+        } else if (roomTypeID == "double-room") {
+            $('#price').val(350);
+            roomName = "Swanson";
+        } else if (roomTypeID == "duplex-room") {
+            $('#price').val(600);
+            roomName = "Brown";
+        } else if (roomTypeID == "cabana-room") {
+            $('#price').val(1000);
+            roomName = "Nexus";
+        } else {
+            $('#price').val(0);
+            roomName = "-";
+        }
         dialog.dialog("open");
     });
 }
