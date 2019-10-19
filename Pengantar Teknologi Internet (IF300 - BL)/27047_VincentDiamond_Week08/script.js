a9d6a16740a5b0ac597405abfcd108b942e38bb7
laitsky <vincent.diamond15@gmail.com>
2019-10-19 17:06:35 +0700

    week08 push

---
+++ Pengantar Teknologi Internet (IF300 - BL)/27047_VincentDiamond_Week08/script.js
@@ -0,0 +1,27 @@
+$(document).ready(documentReady());
+
+function documentReady() {
+    $("#accordion").accordion({
+        collapsible: true
+    });
+
+    let dialog = $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Add new order": function() {
+                //implement here
+            },
+            Cancel: function() {
+                dialog.dialog("close");
+            }
+        }
+    });
+    
+    $(".btnAddOrder").on("click", function(btn) {
+        //and here
+        dialog.dialog("open");
+    });
+}
