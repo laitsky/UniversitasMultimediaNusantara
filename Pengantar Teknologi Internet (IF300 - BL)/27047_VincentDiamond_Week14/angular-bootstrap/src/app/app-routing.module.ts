52b8d89a3d5612cfe8aad24b9080ae438d79cfce
laitsky <vincent.diamond15@gmail.com>
2019-11-22 15:11:35 +0700

    angular+bootstrap update

---
+++ Pengantar Teknologi Internet (IF300 - BL)/27047_VincentDiamond_Week14/angular-bootstrap/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'users',
+    component: UserListComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Pengantar Teknologi Internet (IF300 - BL)/27047_VincentDiamond_Week14/angular-bootstrap/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { HeaderComponent } from './_shared/component/header/header.component';
+import { FooterComponent } from './_shared/component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UserListComponent,
+    UserDetailComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
